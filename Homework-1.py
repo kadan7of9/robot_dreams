19fc670f3e464a4bc5fd9f8866804ddc2a74dba9
Martin Kaduch <kadan7of9@seznam.cz>
2025-10-01 21:50:49 +0200

    Graph update

--- Homework-1.py
+++ Homework-1.py
@@ -12,11 +12,11 @@ print(dataset.head())
 # Rename columns for easier access
 dataset.rename(
     columns={
-        "Frequency": "Frequency",
-        "CurrentSet": "Set_current",
-        "Current": "Real_current",
-        "Voltage Drop": "RSM_voltage_drop",
-        "Crack size": "Crack_size",
+        "Frequency": "Frequency [kHz]",
+        "CurrentSet": "Set current [mA]",
+        "Current": "Real current [mA]",
+        "Voltage Drop": "RSM voltage drop [mV]",
+        "Crack size": "Crack size [mm]",
     },
     inplace=True,
 )
@@ -24,11 +24,11 @@ dataset.rename(
 print(dataset.head())
 
 # Set variables for plotting
-x_axis = "RSM_voltage_drop"
-y_axis = "Crack_size"
-hue = "Real_current"
+x_axis = "RSM voltage drop [mV]"
+y_axis = "Crack size [mm]"
+hue = "Real current [mA]"
 
 plt.figure(figsize=(8, 6))
 sns.scatterplot(data=dataset, x=x_axis, y=y_axis, hue=hue, palette="Spectral")
-plt.title("PTS Crack Meter Data (Seaborn)")
+plt.title("PTS Crack Meter Calibration Data (Seaborn)")
 plt.show()
